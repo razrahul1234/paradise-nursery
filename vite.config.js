210f95bb92000881f5d8d04ea40219b6fb8f1845
Uttam2021 <uttamramankatti@gmail.com>
2025-02-07 23:58:44 +0530

    deploy chnages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    host:true,
-    port:3000
-  },
+  // server:{
+  //   host:true,
+  //   port:3000
+  // },
+  base:"/paradise-nursery",
   plugins: [react()],
 })
